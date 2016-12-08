f1fe4821e9a3031160eb784ebe2ccbe267fe368e
erma <erma@erma-PC>
2016-12-08 00:01:51 -0800

    fungsi perhitungan dasar(pertambahan dan perkalian) pada software netbeans.

---
+++ src/pkg1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1;
+
+/**
+ *
+ * @author erma
+ */
+public class Main {
+ public String a ="erma nur setyani";
+ public String b ="065114132";
+    public void ctk1(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Main erma= new Main();
+         erma.ctk1();
+    }
+    
+    }
+
+
